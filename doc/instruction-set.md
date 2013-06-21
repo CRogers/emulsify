f2153a12252c07140f5296bd9eb33d5a7bd78343
Callum Rogers <callumrogers@hotmail.co.uk>
2013-06-21 22:58:35 +0100

    Implemented some opcodes

--- doc/instruction-set.md
+++ doc/instruction-set.md
@@ -6,14 +6,16 @@ Conceptionally similar to MIPS, all instructions are 32-bits wide. They come in 
 Opcode Layouts
 ---
 
-R: | opcode (6) | rd (5) | rs (5) | rt (5) | sh (6) | func (5) |
-I: | opcode (6) | rd (5) | rs (5) |       immediate (6)        |
+R: | opcode (6) | rd (5) | rs (5) | rt (5) | sh (7) | func (4) |
+   oooooodd dddsssss ttttthhh hhhhffff
+I: | opcode (6) | rd (5) | rs (5) |       immediate (16)       |
+   oooooodd dddsssss iiiiiiii iiiiiiii
 J: | opcode (6) |                address (26)                  |
-
+   ooooooaa aaaaaaaa aaaaaaaa aaaaaaaa
 R-Type
 ---
 
-opcode: 00000000
+opcode: 000000
 
 nop
 00000000 00000000 00000000 00000000
@@ -25,3 +27,24 @@ div: 3
 
 and: 4
 or:  5
+xor: 6
+
+I-Type
+---
+
+opcode:
+	immediate arith: 10ffff
+	memory: 11mmmm
+
+memfunc:
+	lw: 0
+
+J-Type
+---
+
+opcode: 0jjjjj
+
+jfunc:
+	j: 1
+	jr: 2
+	jal: 3
--- src/scripts/machine.ts
+++ src/scripts/machine.ts
@@ -1,14 +1,19 @@
 /// <reference path="main.ts" />
 
+declare var goog;
+
 module Machine {
+
+	var Long = goog.math.Long;
 
 	export enum Register {
 		zero,
 		at,
 		v0, v1,
-		a0, a1, a2, a3, a4, a5, a6, a7,
-		t0, t1, t2, t3, t4, t5,
+		a0, a1, a2, a3,
+		t0, t1, t2, t3, t4, t5, t6, t7,
 		s0, s1, s2, s3, s4, s5, s6, s7,
+		t8, t9,
 		k0, k1,
 		gp,
 		sp,
@@ -16,23 +21,68 @@ module Machine {
 		ra
 	}
 
-	export enum ArithFunc {
-		add,
-		sub,
-		mul,
-		div,
-		and,
-		or
+	export enum RFunc {
+		sll,           // 00
+		_z01 = 0x1, 
+		srl,           // 02
+		sra,           // 03
+		sllv,          // 04
+		_z05 = 0x5,
+		srlv,          // 06
+		srav,          // 07
+		jr,            // 08
+		jalr,          // 09
+		_z0f = 0x0f,
+		mfhi,          // 10
+		_z11 = 0x11,
+		mflo,          // 12
+		_z17 = 0x17,
+		mult,          // 18
+		multu,         // 19
+		div,           // 1a
+		divu,          // 1b
+		_z1a = 0x1a,
+		add,           // 20
+		addu,          // 21
+		sub,           // 22
+		subu,          // 23
+		and,           // 24
+		or,            // 25 
+		xor,           // 26
+		nor,           // 27
+		_z29 = 0x29,
+		slt,           // 2a
+		sltu           // 2b
 	}
 
-	export enum MemFunc {
-		lw,
-		lh,
-		lb,
-		sw,
-		sh,
-		sb,
-		lui
+	export enum Opcode {
+		arith,         // 00
+		_z01 = 0x01,
+		j,             // 02
+		jal,           // 03
+		beq,           // 04
+		bne,           // 05
+		_z07 = 0x07,
+		addi,          // 08
+		addiu,         // 09
+		slti,          // 0a
+		_z0b = 0x0b,
+		andi,          // 0c
+		ori,           // 0d
+		_z0e = 0x0e,
+		lui,           // 0f
+		_z1f = 0x1f,
+		lb,            // 20
+		lh,            // 21
+		_z22 = 0x22,
+		lw,            // 23
+		lbu,           // 24
+		lhu,           // 25
+		_z27 = 0x27,
+		sb,            // 28
+		sh,            // 29
+		_z2a = 0x2a,
+		sw             // 2b
 	}
 
 	export enum InstType {
@@ -40,20 +90,20 @@ module Machine {
 	}
 
 	export class Inst {
-		public instType: InstType;
-		public memFunc: MemFunc;
-		public arithFunc: ArithFunc;
-		public rd: Register;
+		public type: InstType;
+		public opcode: Opcode;
+		public rfunc: RFunc;
 		public rs: Register;
-		public rt: Register;
-		public sh: number;
+		public rt: Register;		
+		public rd: Register;
+		public shamt: number;
 		public immediate: number;
 		public address: number;
 	}
 
 	var B4 = 0xf;
 	var B5 = 0x1f;
-	var B7 = 0x7f;
+	var B6 = 0x3f;
 	var B16 = 0xffff;
 	var B26 = 0x3ffffff;
 
@@ -62,30 +112,24 @@ module Machine {
 
 		var inst = new Inst();
 
-		var opcode = a >> 26;      // oooooo-- = = =
-		inst.rd = (a >> 21) & B5; // ------dd ddd----- = =
-		inst.rs = (a >> 16) & B5; // = ---sssss = =
+		inst.opcode = a >>> 26;    // oooooo-- = = =
+		inst.rs = (a >>> 21) & B5; // ------ss sss----- = =
+		inst.rt = (a >>> 16) & B5; // =        ---ttttt = =
 
 		// Check to see if it's an R-type
-		if (opcode === 0) {
-			inst.instType = InstType.R;
-			inst.rt = (a >> 11) & B5; // = = ttttt--- =
-			inst.sh = (a >> 4) & B7;  // = = -----hhh hhhh----
-			inst.arithFunc = a & B4;        // = = =        ----ffff
+		if (inst.opcode === 0) {
+			inst.type = InstType.R;
+			inst.rd = (a >>> 11) & B5;    // = = ddddd--- =
+			inst.shamt = (a >>> 6) & B5;  // = = -----hhh hh------
+			inst.rfunc = a & B6;          // = = =        --ffffff
 		}
-		else if ((opcode >> 5) === 1) {
-			inst.instType = InstType.I;
-			inst.immediate = a & B16; // = = iiiiiiii iiiiiiii
-			if (((opcode >> 4) & 1) === 1) {
-				inst.memFunc = opcode & B4;
-			}
-			else {
-				inst.arithFunc = opcode & B4;
-			}
+		else if (inst.opcode === 2 || inst.opcode === 3) {
+			inst.type = InstType.J;
+			inst.address = a & B26;
 		}
 		else {
-			inst.instType = InstType.J;
-			inst.address = a & B26;
+			inst.type = InstType.I;
+			inst.immediate = a & B16; // = = iiiiiiii iiiiiiii
 		}
 
 		return inst;
@@ -104,23 +148,82 @@ module Machine {
 
 	export function MachineCtrl($scope: MachineScope) {
 		var PC = new Uint32Array(1);
+		var HI = new Uint32Array(1);
+		var LO = new Uint32Array(1);
 		var memBuf = new ArrayBuffer(128*4);
+		var mem = new Uint8Array(memBuf);
+		var memAs32 = new Uint32Array(memBuf);
+		var reg = new Uint32Array(32);
 
-		$scope.mem = new Uint8Array(memBuf);
-		$scope.memAs32 = new Uint32Array(memBuf);
-		$scope.reg = new Uint32Array(32);
-		$scope.getPC = function()  { return PC[0]; }
-		$scope.setPC = function(v) { PC[0] = v; }
-		$scope.incrPC = function(i) { PC[0] += i; }
-		$scope.Register = Register;
+		function getPC() { return PC[0]; }
+		function setPC(v) { PC[0] = v; }
+		function incrPC(i) { PC[0] += i; }
 
-		$scope.step = function() {
-			var reg = $scope.reg;
-			var mem = $scope.mem;
+		function executeInstruction(i: Inst) {
+			if (i.type === InstType.R) {
+				switch (i.rfunc) {
+					case RFunc.sll: reg[i.rd] = reg[i.rs] << i.shamt; break;
+					case RFunc.srl: reg[i.rd] = reg[i.rs] >> i.shamt; break;
+					case RFunc.sra: reg[i.rd] = reg[i.rs] >>> i.shamt; break; 
+					case RFunc.sllv: reg[i.rd] = reg[i.rs] << reg[i.rt]; break;
+					case RFunc.srlv: reg[i.rd] = reg[i.rs] >> reg[i.rt]; break;
+					case RFunc.srav: reg[i.rd] = reg[i.rs] >>> reg[i.rt]; break;
 
-			$scope.incrPC(1);
-			var val = mem[$scope.getPC()];
+					case RFunc.jr: PC[0] = reg[i.rs]; break;
+					case RFunc.jalr:
+						reg[Register.ra] = PC[0]+4;
+						PC[0] = reg[i.rs];
+						break;
+
+					case RFunc.mfhi: reg[i.rd] = HI[0]; break;
+					case RFunc.mflo: reg[i.rd] = LO[0]; break;
+
+					case RFunc.mult:					
+					case RFunc.multu:
+						var res = new Long(reg[i.rs]).multiply(new Long(reg[i.rt]));
+						LO[0] = res.getLowBits();
+						HI[0] = res.getHighBits();
+						break;
+
+					case RFunc.div:
+					case RFunc.divu:
+						LO[0] = reg[i.rs]/reg[i.rt];
+						HI[0] = reg[i.rs]%reg[i.rt];
+						break;
+
+					case RFunc.addu:
+					case RFunc.add:
+						reg[i.rd] = reg[i.rs] + reg[i.rt]; 
+						break;
+
+					case RFunc.sub:
+					case RFunc.subu:
+						reg[i.rd] = reg[i.rs] - reg[i.rt]; 
+						break;
+
+					case RFunc.and: reg[i.rd] = reg[i.rs] & reg[i.rt]; break;
+					case RFunc.or:  reg[i.rd] = reg[i.rs] | reg[i.rt]; break;
+					case RFunc.xor: reg[i.rd] = reg[i.rs] ^ reg[i.rt]; break;
+					case RFunc.nor: reg[i.rd] = ~(reg[i.rs] | reg[i.rt]); break;
+
+					case RFunc.slt: reg[i.rd] = (reg[i.rs] < reg[i.rt]) ? 1 : 0; break;
+					//case RFunc.sltu: reg[i.rd] = (reg[i.rs] < reg[i.rt]) ? 1 : 0; break;
+				}
+			}		
 		}
+
+
+		function step() {
+			incrPC(4);
+			var val = mem[getPC()];
+		}
+
+		$scope.mem = mem;
+		$scope.memAs32 = memAs32;
+		$scope.reg = reg
+		$scope.getPC = getPC;
+		$scope.Register = Register;
+		$scope.step = step;
 	}
 
 }
